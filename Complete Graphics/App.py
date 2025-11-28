18b1204b043bbd211011b6522fe9728ff8a51717
OnlyGods122 <costomcard221@gmail.com>
2025-11-28 13:02:42 +0530

    Gojo

--- Complete Graphics/App.py
+++ Complete Graphics/App.py
@@ -2,14 +2,23 @@ import turtle
 
 t = turtle.Turtle()
 scr = turtle.Screen()
+scr.setup(1100,700,250,50)
 
-t.teleport(-100,-60)
-for i in range(2):
-    t.forward(200)
-    t.left(90)
-    t.forward(120)
-    t.left(90)
+t.teleport(-100,-30)
+def button(l,b,txt,bg):
+    pos = t.position()
+    t.fillcolor(bg)
+    t.begin_fill()
+    for i in range(2):
+        t.forward(l)
+        t.left(90)
+        t.forward(b)
+        t.left(90)
+    t.end_fill()
+    t.teleport(pos[0]+10,pos[1]+10)
+    t.write(txt,font=("Arial",18,"bold"))
 
+button(200,60,"Welcome to Graphics \n            click me","red")
 
 
 
