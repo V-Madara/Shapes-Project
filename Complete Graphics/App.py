cd4525acfa99e04d92f1aa94043069f7dbe9d8d8
Vishal Kumar <vishalkumarprajapati1881@gmail.com>
2025-11-29 18:03:42 +0530

    gg

--- Complete Graphics/App.py
+++ Complete Graphics/App.py
@@ -5,25 +5,25 @@ scr = turtle.Screen()
 scr.setup(1100,700,250,50)
 
 t.teleport(-100,-30)
-def button(l,b,txt,bg):
+def button(btn_length,btn_breadth,btn_label,btn_bg,text_hight):
     pos = t.position()
-    t.fillcolor(bg)
+    t.fillcolor(btn_bg)
     t.begin_fill()
     for i in range(2):
-        t.forward(l)
+        t.forward(btn_length)
         t.left(90)
-        t.forward(b)
+        t.forward(btn_breadth)
         t.left(90)
     t.end_fill()
     t.teleport(pos[0]+10,pos[1]+10)
-    t.write(txt,font=("Arial",18,"bold"))
+    t.write(btn_label,font=("Arial",text_hight,"bold"))
 
 
-button(200,60,"Welcome to Graphics \n            click me","red")
+button(200,60,"Welcome to Graphics \n            click me","red",13)
 
 def clickPos(x,y):
-    pos = [x,y]
-    return pos
+    if -100<x<100 and -30<y<30:
+        scr.clear()
 
 
 scr.onclick(clickPos)
